e4efddeba8e20b6676aed072e559a3e96198ed9b
kimulegen <kimulegen@proton.me>
2024-07-23 19:20:26 -0400

    feat(practice): add introduction practice file

---
+++ 1_introduction/practice.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+
+//        1. Modifique el codigo para que imprima un mensaje diferente a "Hello World!".
+        System.out.println("Hola Mundo!");
+
+//        2. Cambie la "S" en "System" a minuscula y pruebe compilar
+//        system.out.println("Hello World");
+//        > java: package system does not exist
+
+//        3. Elimine el punto y coma (;) y pruebe a compilar
+//        System.out.println("Hello World!")
+//        > java: ';' expected
+    }
+}
